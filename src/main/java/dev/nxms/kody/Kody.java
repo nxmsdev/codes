656af7a3de6d59280e2b31336b0364c251ac761b
nxms <szymon.lenard@icloud.com>
2026-01-25 11:38:19 +0100

    Initial Java project with Gradle setup

---
+++ src/main/java/dev/nxms/kody/Kody.java
@@ -0,0 +1,17 @@
+package dev.nxms.kody;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Kody extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
